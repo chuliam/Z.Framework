1505949f22b28bdd22c7944ce2af6c3f279ffb46
chuliam <chuliam@chuliam.com>
2018-10-26 11:49:26 +0800

    add queryable extension

--- src/Z.Framework.TestConsole/Program.cs
+++ src/Z.Framework.TestConsole/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 using Z.Framework.Extension;
 
 namespace Z.Framework.TestConsole
@@ -7,6 +9,8 @@ namespace Z.Framework.TestConsole
     {
         static void Main(string[] args)
         {
+            List<string> source = new List<string>();
+            //source = source.AsQueryable().OrderByField("", "").ToList();
             Console.WriteLine("123".SafeToInt32());
             Console.ReadLine();
         }
