8fc72a0000cc563519397ab6a18becfaf827b219
chuliam <chuliam@chuliam.com>
2018-10-26 11:35:26 +0800

    Initial commit and safe convert

---
+++ src/Z.Framework.TestConsole/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Z.Framework.Extension;
+
+namespace Z.Framework.TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("123".SafeToInt32());
+            Console.ReadLine();
+        }
+    }
+}
